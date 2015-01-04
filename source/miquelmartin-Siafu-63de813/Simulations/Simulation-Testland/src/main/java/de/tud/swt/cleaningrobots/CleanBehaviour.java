3bf3552f5f2f0324747d9b6aaa4141a9babf3372
Ronny Marx <karlito1983@googlemail.com>
2015-01-05 00:44:06 +0100

    added missing constructor

--- source/miquelmartin-Siafu-63de813/Simulations/Simulation-Testland/src/main/java/de/tud/swt/cleaningrobots/CleanBehaviour.java
+++ source/miquelmartin-Siafu-63de813/Simulations/Simulation-Testland/src/main/java/de/tud/swt/cleaningrobots/CleanBehaviour.java
@@ -1,6 +1,11 @@
 package de.tud.swt.cleaningrobots;
 
 public class CleanBehaviour extends Behaviour {
+	public CleanBehaviour(Robot robot) {
+		super(robot);
+		// TODO Auto-generated constructor stub
+	}
+
 	private final String CONST_STATE_DIRTY = "Dirty";
 	private final String CONST_STATE_CLEAN = "Clean";
 
